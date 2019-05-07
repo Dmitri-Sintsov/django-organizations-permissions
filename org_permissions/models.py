8834758f4abc4e780e84bebdb23bd2275a5a6186
Dmitri Sintsov <questpc256@gmail.com>
2019-05-07 13:20:06 +0300

    Remove unneeded create_initial arg from OrganizationPermissionManager methods.

--- org_permissions/models.py
+++ org_permissions/models.py
@@ -1,23 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import Permission
-from django.core import management
 
 from organizations.models import Organization
 
 
 class OrganizationPermissionManager(models.Manager):
 
-    def add_user(self, organization_name, user, create_initial=False):
-        if create_initial:
-            management.call_command('create_organizations_permissions')
+    def add_user(self, organization_name, user):
         org, created = Organization.objects.get_or_create(name=organization_name)
         # We will assign organization admin / owner manually in Django Admin.
         org.add_user(user, is_admin=False)
         return org, created
 
-    def remove_user(self, organization_name, user, create_initial=False):
-        if create_initial:
-            management.call_command('create_organizations_permissions')
+    def remove_user(self, organization_name, user):
         org, created = Organization.objects.get_or_create(name=organization_name)
         org.users.remove(user)
         return org, created
