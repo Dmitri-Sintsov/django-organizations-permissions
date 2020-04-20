d0fbfb5d6fa4c9a5b6d343893504391b9cbb8886
relaed <knarfytrebil@gmail.com>
2020-04-21 06:44:31 +0900

    minor fix

--- org_permissions/models.py
+++ org_permissions/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Permission, Group
 from organizations.models import Organization
 
 class OrganizationPermissionManager(models.Manager):
